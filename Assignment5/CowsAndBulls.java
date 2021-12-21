4088f88a02e00d5cea2708554dcc4c8c05e7b3c5
siphosethu1998 <siphosethu1998@gmail.com>
2021-12-21 15:00:18 +0200

    added NumberPicker.java

---
+++ Assignment5/CowsAndBulls.java
@@ -0,0 +1,16 @@
+public class CowsAndBulls {
+	// CowsAndBulls implements the logic for a cows and bulls guessing game. The player has 
+	
+	//	Constants
+	public final static int NUM_DIGITS = 4;
+	public final static int MAX_VALUE = 9876;
+	public final static int MIN_VALUE = 1234;
+	public final static int MAX_GUESSES = 10
+	
+	//	Constructors
+	public CowsAndBulls(int seed) {
+		// Create a CowsAndBulls game using the given randomisation seed value to generate
+		// a mystery number of NUM_DIGITS length, and that gives the player MAX_GUESSES guesses
+
+	}
+}
