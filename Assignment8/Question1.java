1c25fdd9441fb1ba123c3c34350f2b5558e3d94d
siphosethu1998 <siphosethu1998@gmail.com>
2022-03-15 22:50:52 +0200

    Completed and tested Question1.java

--- Assignment8/Question1.java
+++ Assignment8/Question1.java
@@ -5,10 +5,28 @@
 */
 
 import java.util.Scanner;
+import java.util.Queue;
+import java.util.LinkedList;
 
 public class Question1 {
 	public static void main(String[] args){
 		Scanner input = new Scanner(System.in);
+		Queue<String> queue = new LinkedList<String>();
 
+		String line = input.nextLine();
+
+		while(!line.equals("X")){
+
+			if(line.equals("O")) {
+				if(queue.isEmpty()) System.out.printf("Buffer empty %n");
+				else System.out.printf("Data: %s %n",queue.peek());
+				queue.poll();	
+			} else {
+				queue.offer(line);	
+			}
+
+			line = input.nextLine();
+
+		}
 	}
 }
