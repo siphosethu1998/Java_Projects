542808de1ddc0b3111caffa400effac038108a9c
siphosethu1998 <siphosethu1998@gmail.com>
2022-03-15 21:45:09 +0200

    added Question1.java

---
+++ Assignment8/Question1.java
@@ -0,0 +1,14 @@
+/*
+	This program simulates the operation of an I/O buffer using a linked queue data structure
+	Author: Siphosethu Shumani
+	DAte : 15 March 2022
+*/
+
+import java.util.Scanner;
+
+public class Question1 {
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+
+	}
+}
