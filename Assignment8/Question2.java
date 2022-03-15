7a78139a2a08b230255afe13684cf4c75a92177c
siphosethu1998 <siphosethu1998@gmail.com>
2022-03-15 23:04:49 +0200

    added Question2.java

---
+++ Assignment8/Question2.java
@@ -0,0 +1,12 @@
+/*
+	This program identifies mismatched brackets	
+	Author : Siphosethu Shumani
+	Date : 15 March 2022
+*/
+
+
+public class Question2 {
+	public static void main(String[] args){
+		
+	}
+}
